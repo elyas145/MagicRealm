b593207a0bcf8fe323c49c1f6da9de2d4a39e7f8
elyas syoufi <elyas.syoufi@carleton.ca>
2015-02-09 18:15:06 -0500

    hello taylor

--- MagicRealm/src/helloWorld/HelloElyas.java
+++ MagicRealm/src/helloWorld/HelloElyas.java
@@ -4,7 +4,7 @@ public class HelloElyas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello elyas");
+		System.out.println("hello Taylor");
 	}
 
 }
