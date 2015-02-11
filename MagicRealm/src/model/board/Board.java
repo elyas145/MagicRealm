a01fed6c634ec17869364f633535827d2c29c480
elyas syoufi <elyas.syoufi@carleton.ca>
2015-02-10 21:25:46 -0500

    added the chit types, and created the SoundChit class. it gives me an error when i add code in it for some reason...

--- MagicRealm/src/model/board/Board.java
+++ MagicRealm/src/model/board/Board.java
@@ -12,31 +12,24 @@ import java.util.HashSet;
 import java.util.Iterator;
 
 public class Board implements Iterable<HexTile> {
-	private HexTile[][] tiles= new HexTile[5][6]; // based on longest row and longest column
 	private Collection<HexTile> collectionOfTiles;
 	
 	public Board(){
-		hardCodeTiles();
 		collectionOfTiles = new HashSet<HexTile>();
+		hardCodeTiles();
 	}
 	private void hardCodeTiles() {
 		// this setup is based on the picture found here, rotated left to match video:
 		//http://people.scs.carleton.ca/~jeanpier//304W15/Board%20for%20iteration%201/IMG_4841.jpg
 		//I am rotating the actual image files, so they look like they fit together.
-		
-		setTile(TileType.EMPTY, 0, 0);
-		setTile(TileType.EMPTY, 0, 1);
+
 		setTile(TileType.DARK_VALLEY, 0, 2);
 		setTile(TileType.CURST_VALLEY, 0, 3);
-		setTile(TileType.EMPTY, 0, 4);
-		setTile(TileType.EMPTY, 0, 5);
 		
-		setTile(TileType.EMPTY, 1, 0);
 		setTile(TileType.CRAG, 1, 1);
 		setTile(TileType.DEEP_WOODS, 1, 2);
 		setTile(TileType.NUT_WOODS, 1, 3);
 		setTile(TileType.AWFUL_VALLEY, 1, 4);
-		setTile(TileType.EMPTY, 1, 5);
 		
 		setTile(TileType.CLIFF, 2, 0);
 		setTile(TileType.LEDGES, 2, 1);
@@ -49,15 +42,11 @@ public class Board implements Iterable<HexTile> {
 		setTile(TileType.BORDER_LAND, 3, 1);
 		setTile(TileType.BAD_VALLEY, 3, 2);
 		setTile(TileType.CAVES, 3, 3);
-		setTile(TileType.EMPTY, 3, 4);
-		setTile(TileType.EMPTY, 3, 5);
 		
-		setTile(TileType.EMPTY, 4, 0);
 		setTile(TileType.HIGH_PASS, 4, 1);
 		setTile(TileType.CAVERN, 4, 2);
 		setTile(TileType.MOUNTAIN, 4, 3);
-		setTile(TileType.PINE_WOODS, 4, 4);
-		setTile(TileType.EMPTY, 4, 5);		
+		setTile(TileType.PINE_WOODS, 4, 4);	
 	}
 	
 	@Override
@@ -66,8 +55,7 @@ public class Board implements Iterable<HexTile> {
 	}
 	
 	private void setTile(TileType tile, int x, int y) {
-		tiles[y][x] = new HexTile(tile, x, y);
-		collectionOfTiles.add(tiles[y][x]);
+		collectionOfTiles.add(new HexTile(tile, x, y));
 	}
 
 }
---
+++ MagicRealm/src/model/board/ChitType.java
@@ -0,0 +1,26 @@
+package model.board;
+
+public enum ChitType {
+	//sound chits
+	HOWL,
+	FLUTTER,
+	ROAR,
+	PATTER,
+	SLITHER,
+	//warning chits
+	SMOKE,
+	RUINS,
+	DANK,
+	BONES,
+	//site chits
+	STATUE,
+	ALTAR,
+	VAULT,
+	POOL,
+	LOST_CITY,
+	HOARD,
+	LAIR,
+	CAIRNS,
+	SHRINE,
+	LOST_CASTLE
+}
