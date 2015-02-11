04404ce886dc3f2008241deb0611aacc920b1f37
elyas syoufi <elyas.syoufi@carleton.ca>
2015-02-11 11:12:53 -0500

    created the board interface. changed board.java to implement the interface. please add the methods you would like to implement in the board to the interface, I will then implement them. also, add it as a task to Zoho.

---
+++ MagicRealm/src/model/board/BoeardInterface.java
@@ -0,0 +1,21 @@
+/**
+ * This interface lists the public Board functions.
+ * If you need me to implement a function, Please add it here, with a comment describing what goes in, and what should come out.
+ * For clarity and consistancy reasons, we should implement our own classes separatly. 
+ * If you see a mistake in my class, ask me to fix it and vice versa. This way we don't waste time discussing possible solutions, 
+ * since the implementation doesn't really matter as long as it works well.
+ */
+package model.board;
+
+import java.util.Iterator;
+
+public interface BoeardInterface {
+	
+	/**
+	 * @return iterator over the board tiles
+	 */
+	public Iterator<HexTile> iterator();
+	
+	
+	
+}
