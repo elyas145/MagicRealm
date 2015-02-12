82dbef5b9220223b4b9f30fefab4dbba73242b86
elyas syoufi <elyas.syoufi@carleton.ca>
2015-02-12 10:31:25 -0500

    added some initializers to the main controller. started on the character

--- MagicRealm/src/controller/ControllerMain.java
+++ MagicRealm/src/controller/ControllerMain.java
@@ -1,36 +1,59 @@
 package controller;
 
+import java.awt.List;
 import java.io.IOException;
+import java.util.ArrayList;
 
+import config.GameConfiguration;
 import lwjglview.graphics.LWJGLBoardDrawable;
 import lwjglview.graphics.LWJGLGraphics;
 import model.board.Board;
+import model.board.Character;
+import model.board.Treasure;
 import utils.resources.ResourceHandler;
 
 public class ControllerMain {
 	private LWJGLGraphics gfx;
 	private ResourceHandler rh;
+	private Board board;	// main game board
+	private Treasure[] treasures;	// holds a bunch of treasures.
+	private Character[] characters;
+	private int numCharacters;
 	
-	public ControllerMain(){
+	public ControllerMain() {
 		rh = new ResourceHandler();
 		gfx = new LWJGLGraphics(rh);
+		board = new Board();
+		treasures = new Treasure[GameConfiguration.NUMBER_OF_TREASURES];
+		randomTreasures();
 		try {
-			gfx.addDrawable(new LWJGLBoardDrawable(new Board(), rh));
-			
+			gfx.addDrawable(new LWJGLBoardDrawable(board, rh));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
+		}
 	}
-	
-	private void start(){
-		gfx.start();
+
+	private void randomTreasures() {
+		ArrayList<Integer> possibleValues = new ArrayList<Integer>();
+		
+		for(int i = 1; i < 6; i++){
+			possibleValues.add(i*10);
+		}
+		
+		for (int i = 0; i < treasures.length; i++){
+			treasures[i] = new Treasure(possibleValues);
+		}
 		
 	}
+
+	private void start() {
+		gfx.start();
+
+	}
+
 	public static void main(String[] args) throws IOException {
 		ControllerMain main = new ControllerMain();
 		main.start();
 	}
-	
-	
+
 }
---
+++ MagicRealm/src/model/enums/CharacterType.java
@@ -0,0 +1,7 @@
+package model.enums;
+
+public enum CharacterType {
+	CAPTAIN,
+	SWORDSMAN,
+	AMAZON
+}
