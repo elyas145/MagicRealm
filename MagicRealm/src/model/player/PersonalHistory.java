31ef1b35d208150fb10c4ff11163a9ca18e8d38a
elyas syoufi <elyas.syoufi@carleton.ca>
2015-02-17 21:42:02 -0500

    implemented history pad

--- MagicRealm/src/model/player/PersonalHistory.java
+++ MagicRealm/src/model/player/PersonalHistory.java
@@ -1,8 +1,27 @@
 package model.player;
 
+import java.util.ArrayList;
+
 import model.activity.Activity;
 
 public class PersonalHistory {
-
-
+	private ArrayList<PersonalHistoryDay> days;
+	private int currentDay;
+	public PersonalHistory(){
+		days = new ArrayList<PersonalHistoryDay>();
+		currentDay = 1;
+	}
+	
+	public PersonalHistory reset(){
+		days = new ArrayList<PersonalHistoryDay>();
+		currentDay = 1;
+		return this;
+	}
+	
+	public void addActivity(Activity a){
+		days.get(currentDay-1).addActivity(a);
+	}
+	public void newDay(){
+		currentDay++;
+	}
 }
--- MagicRealm/src/model/player/PersonalHistoryDay.java
+++ MagicRealm/src/model/player/PersonalHistoryDay.java
@@ -1,11 +1,44 @@
 package model.player;
 
+import java.util.ArrayList;
+
 import model.activity.Activity;
 
 public class PersonalHistoryDay {
-	private Activity activity;
-	// private MonsterChit monster;
+	private ArrayList<Activity> activities;
 	private int fame;
 	private int notoriety;
 	private int gold;
+
+	public ArrayList<Activity> getActivities() {
+		return activities;
+	}
+
+	public int getFame() {
+		return fame;
+	}
+
+	public void setFame(int fame) {
+		this.fame = fame;
+	}
+
+	public int getNotoriety() {
+		return notoriety;
+	}
+
+	public void setNotoriety(int notoriety) {
+		this.notoriety = notoriety;
+	}
+
+	public int getGold() {
+		return gold;
+	}
+
+	public void setGold(int gold) {
+		this.gold = gold;
+	}
+
+	public void addActivity(Activity a){
+		activities.add(a);
+	}
 }
