7df34e1ca44ca3619a69ec9432243c92a88c0d1f
elyas <elyas@elyas-Lenovo-U310>
2015-03-07 17:26:01 -0500

    networking

--- MagicRealm/src/controller/network/server/handlers/InitBoard.java
+++ MagicRealm/src/controller/network/server/handlers/InitBoard.java
@@ -6,6 +6,7 @@ import network.NetworkHandler;
 import controller.ClientController;
 
 public class InitBoard implements NetworkHandler<ClientController>{
+	private static final long serialVersionUID = 8219666227925319851L;
 	private NetworkHandler<BoardView> board;
 	
 	public InitBoard(NetworkHandler<BoardView> initializer){
