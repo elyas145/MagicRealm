d64cb6a9649ed714e3b3cb0f9a179c6902269b41
elyas syoufi <elyas.syoufi@carleton.ca>
2015-03-30 19:43:03 -0400

    asdf

---
+++ MagicRealm/src/communication/handler/client/SetSwordsmanPlay.java
@@ -0,0 +1,20 @@
+package communication.handler.client;
+
+import server.ServerController;
+import communication.ServerNetworkHandler;
+
+public class SetSwordsmanPlay implements ServerNetworkHandler {
+	private static final long serialVersionUID = 6619288721160628363L;
+	private boolean turn;
+
+	public SetSwordsmanPlay(boolean turn) {
+		this.turn = turn;
+	}
+
+	@Override
+	public void handle(ServerController controller) {
+		controller.setSwordsManTurn(turn);
+		controller.notify();
+	}
+
+}
--- MagicRealm/src/communication/handler/server/CheckSwordsmanPlay.java
+++ MagicRealm/src/communication/handler/server/CheckSwordsmanPlay.java
@@ -5,7 +5,6 @@ import communication.ClientNetworkHandler;
 
 public class CheckSwordsmanPlay implements ClientNetworkHandler {
 	private static final long serialVersionUID = -8174217892766593866L;
-
 	@Override
 	public void handle(ClientController controller) {
 		controller.checkSwordsmanTurn();
