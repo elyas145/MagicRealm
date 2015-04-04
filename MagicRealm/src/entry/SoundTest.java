21494a159a6ca245924d392e0325929a23764aa9
taylor <_blank_@live.ca>
2015-04-04 17:59:09 -0400

    Commiting changes

--- MagicRealm/src/entry/SoundTest.java
+++ MagicRealm/src/entry/SoundTest.java
@@ -1,21 +1,17 @@
 package entry;
 
-import view.audio.SoundController;
 import jogamp.audio.JogAmpAudio;
-import jogamp.audio.JogAmpSoundController;
 
 public class SoundTest {
 
 	public static void main(String[] args) throws InterruptedException {
 		JogAmpAudio sounds = JogAmpAudio.getInstance();
-		sounds.playSound("BeforeTheBattle.wav");
+		sounds.playSound("VictoryMarch.wav");
 		Thread.sleep(2000);
-		//sounds.playSound("Mists.wav");
-		//Thread.sleep(5000);
-		sounds.playSound("Highlander.wav");
-		Thread.sleep(2000);
-		sounds.playSound("spell_fury_final_mix.wav");
-		Thread.sleep(30000);
+		for(int i = 0; i < 10; ++i) {
+			sounds.playSound("alert1.wav");
+			Thread.sleep(2000);
+		}
 		sounds.killALData();
 	}
 
